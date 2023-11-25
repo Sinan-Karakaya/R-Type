68d2a769e6911464f30f59f07a8702b9e799f96a
Sinan Karakaya <sinan.karakaya@epitech.eu>
2023-11-25 19:32:15 +0100

    fix(editor): fixed crash before main

--- editor/src/editor.cpp
+++ editor/src/editor.cpp
@@ -8,27 +8,25 @@
 #include "Editor.hpp"
 #include "App/App.hpp"
 
-project_t g_projectInfos = { 0 };
+project_t g_projectInfos = {"", "", false, false};
 
 #ifdef _WIN32
-    int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-    {
-        (void)hInstance;
-        (void)hPrevInstance;
-        (void)lpCmdLine;
-        (void)nCmdShow;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    (void)hInstance;
+    (void)hPrevInstance;
+    (void)lpCmdLine;
+    (void)nCmdShow;
 
-        RType::Editor::App app;
-        app.Run();
+    RType::Editor::App app;
+    app.Run();
 
-        return 0;
-    }
+    return 0;
+}
 #else
-    int main() 
-    {
-        RType::Editor::App app;
-        app.Run();
+int main() {
+    RType::Editor::App app;
+    app.Run();
 
-        return 0;
-    }
+    return 0;
+}
 #endif
