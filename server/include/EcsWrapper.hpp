3ead78c859bbb5122d56dae2e2347f588a492d27
JasonLAGOUTE <jason.lagoute@epitech.eu>
2023-12-07 14:32:08 +0100

    feat(EcsWrapper): starting the Wrapper for the ECS

---
+++ server/include/EcsWrapper.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "RType.hpp"
+
+namespace RType
+{
+    class EcsWrapper
+    {
+    public:
+        EcsWrapper();
+        ~EcsWrapper();
+        void createEntity(int entityId, /*??? EntityType,*/ int posX, int posY); // RFC: 4.7 PacketEntitySpawn
+        void deleteEntity(int entityId);
+        void moveEntity(int entityId, int posX, int posY, int dirX, int dirY);
+        void hitEntity(int entityId);
+        void launchBullet( /*??? BulletType,*/ int posX, int posY, int dirX, int dirY); // RFC: 4.11 PacketPlayerLaunchBullet
+        void playerDie(int entityId);
+        void playerSpawn(int entityId, int posX, int posY/*, color*/); // RFC: 4.5 PacketPlayerSpawn
+        void playerHitEntity(int entityId, int score);
+        void helloServer(); // RFC: 4.1 PacketHelloServer
+
+    private:
+        RType::Runtime::ECS::Registry m_registry;
+    };
+}; // namespace RType
