60ba16c9654eac487953fa0d4b94f9587ef881bf
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-12-18 10:30:39 +0100

    build(macos): disable sfml-audio and openal on macos

--- client/src/rtype.cpp
+++ client/src/rtype.cpp
@@ -39,6 +39,61 @@ int main()
     runtime->Init();
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "RType");
 
+    /*
+    auto rectangle = sf::RectangleShape(sf::Vector2f(100, 100));
+    rectangle.setFillColor(sf::Color::Red);
+    RType::Runtime::ECS::Components::UIRectangleElement uiRectangle = {
+        .rectangle = rectangle,
+        .text = "Hello, World"
+        // .text = sf::Text("Hello World", sf::Font::GetDefaultFont(), 50)
+    };
+    */
+    
+    auto &registry = runtime->GetRegistry();
+
+    sf::CircleShape circle(30);
+    circle.setOutlineThickness(10);
+    circle.setOutlineColor(sf::Color(250, 150, 100));
+    circle.setFillColor(sf::Color(150, 50, 250));
+    circle.setPosition(500, 650);
+
+
+    RType::Runtime::ECS::Entity circleEntity = runtime->AddEntity();
+
+    registry.AddComponent(circleEntity, RType::Runtime::ECS::Components::CircleShape {
+        .circle = circle
+    });
+
+
+    sf::RectangleShape rectangle(sf::Vector2f(100, 100));
+    rectangle.setOutlineColor(sf::Color(250, 150, 100));
+    rectangle.setFillColor(sf::Color(150, 50, 250));
+    rectangle.setPosition(0, 0);
+
+    sf::Font font;
+
+    font.loadFromFile("assets/Roboto.ttf");
+
+    sf::Text text("Hello World", font);
+    text.setPosition(100, 150);
+    text.setFillColor(sf::Color::Red);
+    text.setCharacterSize(30);
+
+    RType::Runtime::ECS::Entity rectangleEntity = runtime->AddEntity();
+
+    registry.AddComponent(rectangleEntity, RType::Runtime::ECS::Components::Transform {
+        .position = { 0, 0 },
+        .rotation = {0, 0},
+        .scale = { 1, 1 }
+    });
+
+    registry.AddComponent(rectangleEntity, RType::Runtime::ECS::Components::UIRectangleElement {
+        .rectangle = rectangle,
+        .text = text
+    });
+
+
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
