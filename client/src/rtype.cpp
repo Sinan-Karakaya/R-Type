c8ad15e394e3215d8bdeb95c0b9149ed0a9d085d
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-12-19 10:35:46 +0100

    feat(client): create client main

--- client/src/rtype.cpp
+++ client/src/rtype.cpp
@@ -5,102 +5,31 @@
 ** client
 */
 
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-
+#include "Client.hpp"
 #include "Modules/modules.hpp"
 #include "RType.hpp"
-#include "Runtime/IRuntime.hpp"
 
-RType::Runtime::IRuntime *RuntimeEntry();
-void RuntimeDestroy(RType::Runtime::IRuntime *runtime);
-
-int main()
+int main(int ac, char **av)
 {
-    void *libHandle = RType::Utils::Modules::LoadSharedLibrary("runtime");
-    if (!libHandle) {
-        RTYPE_LOG_CRITICAL("Failed to load runtime library");
+    if (
+        (ac == 2 && (std::string(av[1]) == "--help" || std::string(av[1]) == "-h")) ||
+        (ac != 3)
+    ) {
+        std::cout << "Usage: ./client [IP] [PORT]" << std::endl;
+        std::cout << "Ex. ./client 127.0.0.1 4242" << std::endl;
         return 84;
     }
 
-    RType::Runtime::IRuntime *(*runtimeEntry)() =
-        (RType::Runtime::IRuntime * (*)()) RType::Utils::Modules::GetFunction(libHandle, "RuntimeEntry");
-    if (!runtimeEntry) {
-        RTYPE_LOG_CRITICAL("Failed to get runtime entry point");
+    std::string ip = av[1];
+    short port = static_cast<short>(std::stoi(av[2]));
+    std::unique_ptr<RType::Client::Client> client;
+
+    try {
+        client = std::make_unique<RType::Client::Client>(ip, port);
+    } catch (std::exception &e) {
+        CLIENT_LOG_ERROR("Exception caught during start: {0}", e.what());
         return 84;
     }
-
-    RType::Runtime::IRuntime *runtime = runtimeEntry();
-    if (!runtime) {
-        RTYPE_LOG_CRITICAL("Failed to create runtime instance");
-        return 84;
-    }
-
-    runtime->Init();
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "RType");
-
-    /*
-    auto rectangle = sf::RectangleShape(sf::Vector2f(100, 100));
-    rectangle.setFillColor(sf::Color::Red);
-    RType::Runtime::ECS::Components::UIRectangleElement uiRectangle = {
-        .rectangle = rectangle,
-        .text = "Hello, World"
-        // .text = sf::Text("Hello World", sf::Font::GetDefaultFont(), 50)
-    };
-    */
-
-    auto &registry = runtime->GetRegistry();
-
-    sf::CircleShape circle(30);
-    circle.setOutlineThickness(10);
-    circle.setOutlineColor(sf::Color(250, 150, 100));
-    circle.setFillColor(sf::Color(150, 50, 250));
-    circle.setPosition(500, 650);
-
-    RType::Runtime::ECS::Entity circleEntity = runtime->AddEntity();
-
-    registry.AddComponent(circleEntity, RType::Runtime::ECS::Components::CircleShape {.circle = circle});
-
-    sf::RectangleShape rectangle(sf::Vector2f(100, 100));
-    rectangle.setOutlineColor(sf::Color(250, 150, 100));
-    rectangle.setFillColor(sf::Color(150, 50, 250));
-    rectangle.setPosition(0, 0);
-
-    sf::Font font;
-
-    font.loadFromFile("assets/Roboto.ttf");
-
-    sf::Text text("Hello World", font);
-    text.setPosition(100, 150);
-    text.setFillColor(sf::Color::Red);
-    text.setCharacterSize(30);
-
-    RType::Runtime::ECS::Entity rectangleEntity = runtime->AddEntity();
-
-    registry.AddComponent(rectangleEntity, RType::Runtime::ECS::Components::Transform {
-                                               .position = {0, 0}, .rotation = {0, 0}, .scale = {1, 1}});
-
-    registry.AddComponent(rectangleEntity,
-                          RType::Runtime::ECS::Components::UIRectangleElement {.rectangle = rectangle, .text = text});
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-                window.close();
-            if (event.type == sf::Event::Resized)
-                runtime->HandleResizeEvent(event);
-        }
-
-        window.clear();
-
-        runtime->Update(event);
-        runtime->Render();
-
-        window.draw(runtime->GetRenderTextureSprite());
-
-        window.display();
-    }
-
+    client->run();
     return 0;
 }
