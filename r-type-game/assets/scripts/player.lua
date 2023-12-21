cba6e685f624ef92d451cb9c1b4856c252d0e988
Sinan-Karakaya <sinan.karakaya@epitech.eu>
2023-12-21 22:09:19 +0100

    feat(Runtime): add dev branch to release workflow and update player.lua

--- r-type-game/assets/scripts/player.lua
+++ r-type-game/assets/scripts/player.lua
@@ -4,27 +4,11 @@ function update(e)
     local seconds = getElapsedTimeAnimation(e)
     local drawable = getDrawable(e)
 
-    -- debug floatRect
-    local floatRect = floatRect(0.1,0.2,0.3,0.4)
-    print("floatRect.left: " .. floatRect.left)
-    print("floatRect.top: " .. floatRect.top)
-    print("floatRect.width: " .. floatRect.width)
-    print("floatRect.height: " .. floatRect.height)
-    -- debug floatRect
-    print("player update")
-    print("seconds: " .. seconds)
-    print("drawable.frameDuration: " .. drawable.frameDuration)
-    print("drawable.isAnimated: " .. tostring(drawable.isAnimated))
-    print("drawable.currentFrame: " .. drawable.currentFrame)
-    print("drawable.frameCount: " .. drawable.frameCount)
-    print("drawable.floatRect.left: " .. drawable.floatRect.left)
-    print("drawable.floatRect.width: " .. drawable.floatRect.width)
-
     ---- handle animation ----
     if seconds > drawable.frameDuration and drawable.isAnimated then
         restartClockAnimation(e)
         drawable.currentFrame = drawable.currentFrame + 1
-        drawable.floatRect.left = drawable.floatRect.width * drawable.currentFrame
+        drawable.floatRect.left = drawable.floatRect.left + drawable.leftDecal
         if drawable.currentFrame > drawable.frameCount then
             drawable.currentFrame = 0
         end
@@ -64,7 +48,6 @@ end
 
 function onCollision(e, other)
     local tagOther = getComponentTag(other)
-    -- print(tagOther)
     if tagOther == "enemy" then
         destroyEntity(other)
     end
