d1ec1f655ed4714db302f22e1c93917ab0443f85
jovanHillion <jovanhillion@gmail.com>
2024-01-03 17:08:58 +0100

    feat(./common/Graphics/RectangleShape.hpp): add rectangleShape for abstraction

---
+++ common/Graphics/RectangleShape.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type [WSL: Ubuntu-22.04]
+** File description:
+** RectangleShape
+*/
+
+#pragma once
+
+#include "Utils.hpp"
+
+namespace RType::Graphics
+{
+    class RectangleShape {
+        public:
+            RectangleShape();
+            RectangleShape(const Vector2f &size = Vector2f(0, 0));
+            ~RectangleShape();
+
+            virtual void setSize(const Vector2f &size);
+            virtual const Vector2f &getSize() const;
+            virtual Vector2f getPoint(std::size_t index) const;
+    };
+} // namespace RType::Graphics
