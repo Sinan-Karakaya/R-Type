503161526f7e00ed312f939ada77250dd37787a3
Sinan <sinan.karakaya@epitech.eu>
2024-01-10 12:10:42 +0100

    feat(Runtime): random pitch modifier on play sound

--- runtime/src/Runtime.cpp
+++ runtime/src/Runtime.cpp
@@ -174,6 +174,7 @@ namespace RType::Runtime
                 static sf::Sound s(sound);
                 auto &transform = m_registry.GetComponent<RType::Runtime::ECS::Components::Transform>(e);
                 s.setPosition(sf::Vector3f(transform.position.x, transform.position.y, 0));
+                s.setPitch(RType::Utils::Random::GetFloat(0.8f, 1.2f));
                 s.play();
             })
         });
